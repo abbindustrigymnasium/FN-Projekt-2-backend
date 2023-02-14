0ea145c37af5eca74dc84a382f4ef02fa5dba11b
abbseblin <sebastian.ling@abbgymnasiet.se>
2023-02-14 10:20:15 +0100

    made code pretty and not so messy

---
+++ Data/DevData.ts
@@ -0,0 +1,29 @@
+const DevData = [
+            { name: 'Sebastian Ling',
+              age: 17, 
+              duties: 'Backend/prisma developer',
+              ImageURL: 'URLexample'
+            },
+            { name: 'Viktor Ramic Simic',
+              age: 17, 
+              duties: 'Backend/prisma developer',
+              ImageURL: 'URLexample'
+            },
+            { name: 'Olle Thor',
+              age: 17, 
+              duties: 'Backend/prisma developer',
+              ImageURL: 'URLexample'
+            },
+            { name: 'Cecilia Ek',
+              age: 17, 
+              duties: 'Frontend developer/designer',
+              ImageURL: 'URLexample'
+            },
+            { name: 'Emilia Romberg',
+              age: 17, 
+              duties: 'Frontend developer/designer',
+              ImageURL: 'URLexample'
+            },
+          ]
+
+export {DevData}
---
+++ Data/userData.ts
@@ -0,0 +1,9 @@
+const userData = [
+    {
+        Username: "JohnDoe123",
+        Password: "123456789",
+        ProfileImageURL: "https://www.shutterstock.com/image-vector/male-figure-man-boy-profile-260nw-1647374107.jpg",
+    }
+]
+
+export {userData}
